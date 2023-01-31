8a0a375f79b46aedaab76c7da753a86589b25390
Ariouz <55002955+Ariouz@users.noreply.github.com>
2023-01-31 19:59:07 +0100

    c01:ex00

---
+++ exercices/C01/ex00/ft_ft.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+void ft_putchar(char a){
+    write(1, &a, 1);
+}
+
+void ft_putnbr(int nb){
+    
+    if(nb < 0){
+        ft_putchar('-');
+        nb = -nb;
+    }
+
+    if(nb > 9){
+        int d = nb /10;
+        ft_putnbr(d);
+    }
+
+    int mod = nb%10;
+    if(nb > 9){
+        ft_putchar(mod+'0');
+    }else{
+        ft_putchar(nb+'0');
+    }
+}
+
+void ft_ft(int *nbr){
+    *nbr = 42;
+    ft_putnbr(*nbr);
+}
+
+int main(){
+    int c = 4;
+    int *a = &c;
+    ft_ft(a);
+    return 0;
+}
